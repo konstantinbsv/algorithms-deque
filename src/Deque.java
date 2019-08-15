2787c464d0c8a2caf1a88321ca88c2aa8cc63f74
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-08-15 15:09:15 -0700

    Implement add and remove methods for array-based deque

--- src/Deque.java
+++ src/Deque.java
@@ -1,31 +1,82 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class Deque<Item> implements Iterable<Item> {
+    private static final int INITIAL_CAPACITY = 16;
+
+    private int top = INITIAL_CAPACITY/2;
+    private int bottom = INITIAL_CAPACITY/2;
+    private int capacity = INITIAL_CAPACITY;
+
+    private Object[] deque;
 
     // construct an empty deque
-    public Deque()
+    public Deque() {
+         deque = new Object[INITIAL_CAPACITY];
+    }
 
     // is the deque empty?
-    public boolean isEmpty()
+    public boolean isEmpty() {
+        return top == bottom;
+    }
 
     // return the number of items on the deque
-    public int size()
+    public int size() {
+        return top - bottom;
+    }
 
     // add the item to the front
-    public void addFirst(Item item)
+    public void addFirst(Item item) {
+        if (item == null) throw new IllegalArgumentException();
+        if (top == capacity) doubleCapacity();
+
+        deque[top] = item;
+        top++;
+    }
 
     // add the item to the back
-    public void addLast(Item item)
+    public void addLast(Item item) {
+        if (item == null) throw new IllegalArgumentException();
+        if (bottom == 0) doubleCapacity();
+
+        bottom--;
+        deque[bottom] = item;
+    }
 
     // remove and return the item from the front
-    public Item removeFirst()
+    public Item removeFirst() {
+        if (isEmpty()) throw new NoSuchElementException();
+
+        Item firstItem;
+        top--; // move back one spot to first item
+        firstItem = (Item) deque[top]; // get first item
+        deque[top] = null; // prevent loitering
+        return firstItem;
+    }
 
     // remove and return the item from the back
-    public Item removeLast()
+    public Item removeLast() {
+        if (isEmpty()) throw new NoSuchElementException();
+
+        Item lastItem;
+        lastItem = (Item) deque[bottom];
+        deque[bottom] = null;
+        bottom++;
+        return lastItem;
+    }
+
+    private void doubleCapacity() {
+    }
 
     // return an iterator over items in order from front to back
-    public Iterator<Item> iterator()
+    public Iterator<Item> iterator() {
+
+
+    }
 
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+
 
 }
 
