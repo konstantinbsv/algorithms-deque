247b4b2e8bc5574eb5df64620dbddf28189f538b
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-08-15 17:22:08 -0700

    Bug fix: wrong method call

--- src/Permutation.java
+++ src/Permutation.java
@@ -3,11 +3,11 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
     public static void main(String[] args) {
-        int k = Integer.getInteger(args[0]); // number of strings to print
+        int k = Integer.parseInt(args[0]); // number of strings to print
 
         RandomizedQueue randQueue = new RandomizedQueue();
         
-        while(!StdIn.isEmpty()) {
+        while (!StdIn.isEmpty()) {
             randQueue.enqueue(StdIn.readString());
         }
 
